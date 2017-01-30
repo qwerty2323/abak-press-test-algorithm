a86f8acb8cd900bba4f88a4ebe10847be3b854c7
Ivan Shelomentsev <Shelomentsev.Ivan@gmail.com>
2017-01-30 16:43:53 +0500

    Modified binary

--- lib/binary.rb
+++ lib/binary.rb
@@ -7,16 +7,12 @@ module Binary
 
   def search(value, array=data)
     mid = array.size/2
-    puts 'found mid'
     n = array[mid]
-    puts 'assigned n = array[mid].age'
     if n.age == value
       n
     elsif n.age < value
-      puts 'passed first condition'
       search(value, array.drop(mid))
     elsif n.age > value
-      puts 'passed second condition'
       search(value, array.take(mid))
     else
       "No results"
